1f91cd9986769e831a432a37410fcc79d23a8f16
Gal <galg@elad.co.il>
2026-04-30 10:20:52 +0300

    project setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Show, SignOutButton, SignUpButton} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-   <Button>Click me</Button>
+  <div>
+    <h1>Home Page</h1>
+
+
+    <Show when="signed-out">
+      <SignUpButton mode="modal">Sign Up</SignUpButton>
+    </Show>
+
+    <Show when="signed-in">
+      <SignOutButton>Signout</SignOutButton>
+    </Show>
+  </div>
   );
 }
